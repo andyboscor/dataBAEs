de023177b303910424a1eb4cb82e2f8aa9436514
Andrei Boscor <andyboscor@yahoo.com>
2017-01-28 15:31:44 +0000

    Toolbar add

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import Dashboard from './Dashboard';
 
 class App extends Component {
 
   render() {
     return (
       <MuiThemeProvider>
-   <MyAwesomeReactComponent />
+   <Dashboard />
  </MuiThemeProvider>
     );
   }
---
+++ src/Dashboard.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
+import AutoComplete from 'material-ui/AutoComplete';
+
+class Login extends Component {
+static muiName = 'FlatButton';
+
+  render() {
+    return (
+      <FlatButton {...this.props} label="Login" />
+    );
+  }
+}
+
+const Logged = (props) => (
+  <IconMenu
+    {...props}
+    iconButtonElement={
+      <IconButton><MoreVertIcon /></IconButton>
+    }
+    targetOrigin={{horizontal: 'right', vertical: 'top'}}
+    anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+  >
+    <MenuItem primaryText="Refresh" />
+    <MenuItem primaryText="Help" />
+    <MenuItem primaryText="Sign out" />
+  </IconMenu>
+);
+
+Logged.muiName = 'IconMenu';
+
+/**
+ * This example is taking advantage of the composability of the `AppBar`
+ * to render different components depending on the application state.
+ */
+class Dashboard extends Component {
+  state = {
+    logged: true,
+  };
+
+  handleChange = (event, logged) => {
+    this.setState({logged: logged});
+  };
+
+  render() {
+    return (
+      <div>
+        <Toggle
+          label="Logged"
+          defaultToggled={true}
+          onToggle={this.handleChange}
+          labelPosition="right"
+          style={{margin: 20}}
+        />
+        <AppBar
+          title="FriendZone"
+          iconElementLeft={<IconButton><NavigationMenu /></IconButton>}
+          iconElementRight={this.state.logged ? <Logged /> : <Login />}
+        />
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
