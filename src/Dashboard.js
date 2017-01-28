deaa6ea3a5e2cafc8a1b4da0c2b356daa4ff9de4
arvikachristy <anastasia.arvika@gmail.com>
2017-01-28 15:56:20 +0000

    Adding menu list toolbar :jack_o_lantern:

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,6 +8,7 @@ import Toggle from 'material-ui/Toggle';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import NavigationMenu from 'material-ui/svg-icons/navigation/menu';
 import AutoComplete from 'material-ui/AutoComplete';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 class Login extends Component {
 static muiName = 'FlatButton';
@@ -33,6 +34,16 @@ const Logged = (props) => (
     <MenuItem primaryText="Sign out" />
   </IconMenu>
 );
+
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400,
+  },
+};
+
 
 Logged.muiName = 'IconMenu';
 
@@ -64,6 +75,53 @@ class Dashboard extends Component {
           iconElementLeft={<IconButton><NavigationMenu /></IconButton>}
           iconElementRight={this.state.logged ? <Logged /> : <Login />}
         />
+
+        <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}>
+
+        <Tab label="Blog" value="a" >
+          <div>
+            <h2 style={styles.headline}>Controllable Tab A</h2>
+            <p>
+              Tabs are also controllable if you want to programmatically pass them their values.
+              This allows for more functionality in Tabs such as not
+              having any Tab selected or assigning them different values.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Friend List" value="b">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Circles" value="c">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>        
+     
+        <Tab label="Messaging" value="d">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>      
+ </Tabs>
       </div>
     );
   }
