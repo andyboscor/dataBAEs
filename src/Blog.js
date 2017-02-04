46cfc2ec4a387901c776bba7b89ec28aae0c311c
Andrei Boscor <andyboscor@yahoo.com>
2017-02-04 00:31:12 +0000

    Created blog posts with states

--- src/Blog.js
+++ src/Blog.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
-
-
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Post from './Post.js';
+var addBottom = {
+  marginBottom: '50px'
+}
+  var cardarray = [ {title:"This is my long ass post", message: "I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I could've handled the states."}, {title:"This is my long ass post", message:"  I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I could've handled the states."}, {title:'This is my long ass post', message:"  I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I couldve handled the states and some other shit."}];
 class Blog extends Component {
 
 
   render() {
 
     return (
-    <div>
+    <div style={addBottom}>
+    {
+      cardarray.map(function(item, i){
+          return <Post key={i} {...item} />
+        },this)}
       </div>
     );
   }
---
+++ src/Post.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+var blogCards = {
+  marginLeft: '20vw',
+  marginRight: '20vw',
+  marginTop: '5vh',
+
+}
+
+var headerStyle ={
+    backgroundColor: '#3b5999',
+}
+class Post extends Component {
+
+
+  render() {
+
+    return (
+    <div>
+    <Card style={blogCards}>
+    <CardHeader
+      title={this.props.title}
+      actAsExpander={true}
+      titleColor="white"
+      style={headerStyle}
+    />
+    <CardText>
+      {this.props.message}
+    </CardText>
+    <CardActions>
+    </CardActions>
+
+  </Card>
+      </div>
+    );
+  }
+}
+
+export default Post;
