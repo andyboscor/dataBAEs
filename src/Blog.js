ec3c53c2d4612bbd9c3157a09eec1aae1b834488
arvikachristy <anastasia.arvika@gmail.com>
2017-02-07 18:44:41 +0000

    Add blog posts :octopus:

--- src/Blog.js
+++ src/Blog.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import Post from './Post.js';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import 'whatwg-fetch';
+
 var addBottom = {
   marginBottom: '50px'
 }
 
-  var cardarray1 = [ {username:"This is my long ass post", email_address: "I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I could've handled the states."}, {username:"This is my long ass post", email_address:"  I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I could've handled the states."}, {username:'This is my long ass post', email_address:"  I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I couldve handled the states and some other shit."}];
+var cardarray1 = [ {username:"This is my long ass post", email_address: "I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I could've handled the states."}, {username:"This is my long ass post", email_address: "I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thi"}, {username:"This is my long ass post", email_address:"  I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I could've handled the states."}, {username:'This is my long ass post', email_address:"  I don't think I'm into this whole Computer Science thing anymore. Writing code. What the hell was I thinking when I signed up for this. Like duuuh. But like it's cool tho. Look at me go I've already wasted so much time writing this when I couldve handled the states and some other shit."}];
 
+
+var buttonFloat ={
+  marginRight: 20,
+  display: 'flex', 
+  justifyContent: 'center'
+};
 
 
 class Blog extends Component {
@@ -16,6 +28,7 @@ class Blog extends Component {
   state = {
     cardarray: []
   }
+
   componentDidMount() {
     var self = this;
     fetch('http://friendzone.azurewebsites.net/API.php/friends/1', {
@@ -36,15 +49,55 @@ class Blog extends Component {
     self.setState({cardarray: cardarray1})
     }
 
+  stateButton = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
+  handleClose = () => {
+    this.setState({open: false});
+  };
+
+
   render() {
+
+    const actions = [
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleClose}
+      />,
+      <FlatButton
+        label="Submit"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
 
     return (
     <div style={addBottom}>
-    {
-      this.state.cardarray.map(function(item, i){
-          return <Post key={i} {...item} />
-        },this)}
+      <div style={buttonFloat}>
+        <FloatingActionButton onTouchTap={this.handleOpen}>
+          <ContentAdd/>
+          <Dialog
+            actions={actions}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose}
+            autoScrollBodyContent={true}
+          >
+          </Dialog>
+        </FloatingActionButton>
       </div>
+
+      {this.state.cardarray.map(function(item, i){
+        return <Post key={i} {...item} />},this)}
+
+    </div>
     );
   }
 }
